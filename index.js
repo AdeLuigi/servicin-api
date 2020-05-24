648450446bce62bf0cc6c69ce73e37f2a235c222
adeluigi <ademario63@gmail.com>
2020-05-24 05:59:02 -0300

    implementacao do cors para fazer requisicao

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes/index');
 
 const app = express();
@@ -10,6 +11,7 @@ mongoose.connect(
     useUnifiedTopology: true,
   },
 );
+app.use(cors());
 app.use(routes);
 // app.use(express.json());
 
